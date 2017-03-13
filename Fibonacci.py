40f2b1fda78f558b01af89af08a4766198075902
Pastor E. Ortega G <pastortega@gmail.com>
2017-03-13 17:27:57 -0300

    Generar Serie Fibonacci
    Esta es una Versión Simple de Generar la Serie Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,21 @@
+
+
+# Mi Version 1.0
+print("\n Bienvenido al Generador de Series Fibonacci \n\n")
+# Ingreso del Limite de la Serie
+Limite = int( imput( "Ingrese el Limite :"))
+Elemento = 1
+Inicio = 0
+Paso = 1
+print ' 0, 1', 
+
+while ( Inicio + Paso < Limite ):
+	Elemento = Inicio + Paso
+	Inicio = Paso
+	Paso = Elemento
+	print ', ', Elemento ,
+
+
+print '.'
+print Elemento , ' < ' , Limite 
+
